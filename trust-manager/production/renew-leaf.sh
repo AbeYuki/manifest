85c0dff2d8594be87281ec497cb235cdbacae45f
AbeYuki <aim4highergg@gmail.com>
2026-05-02 11:43:49 +0900

    mod

--- trust-manager/production/renew-leaf.sh
+++ trust-manager/production/renew-leaf.sh
@@ -6,5 +6,25 @@ kubectl cert-manager renew -n argocd avp-client-leaf
 kubectl cert-manager renew -n vault traefik-vault-client-leaf
 
 # 2. 再起動
-kubectl -n vault rollout restart statefulset/vault
+kubectl -n vault delete pod vault-2
+until kubectl -n vault get pod vault-2 >/dev/null 2>&1; do
+  echo "waiting for vault-2 to be recreated..."
+  sleep 2
+done
+kubectl -n vault wait --for=condition=PodScheduled pod/vault-2 --timeout=300s
+
+kubectl -n vault delete pod vault-1
+until kubectl -n vault get pod vault-1 >/dev/null 2>&1; do
+  echo "waiting for vault-1 to be recreated..."
+  sleep 2
+done
+kubectl -n vault wait --for=condition=PodScheduled pod/vault-1 --timeout=300s
+
+kubectl -n vault delete pod vault-0
+until kubectl -n vault get pod vault-0 >/dev/null 2>&1; do
+  echo "waiting for vault-0 to be recreated..."
+  sleep 2
+done
+kubectl -n vault wait --for=condition=PodScheduled pod/vault-0 --timeout=300s
+
 kubectl -n argocd rollout restart deploy/argo-cd-argocd-repo-server
